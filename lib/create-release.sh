72da2ff53b5840fe0cf885183293bbdd8a5b322c
Nathan Kramer <nathan.l.kramer@gmail.com>
2017-09-29 11:09:49 +1000

    Fix bad jq arguments

--- lib/create-release.sh
+++ lib/create-release.sh
@@ -4,7 +4,7 @@ readonly release_endpoint="${GITHUB_API_URL:-https://api.github.com}/repos/%s/re
 readonly release_json='{"tag_name": "%s", "name": "%s", "target_commitish": "%s", "body": "%s"}'
 
 function latest-release-tag() {
-	jq ".tag_name" <(curl -s -X GET "$1/latest?access_token=$GITHUB_ACCESS_TOKEN")
+	jq -r ".tag_name" <(curl -s -X GET "$1/latest?access_token=$GITHUB_ACCESS_TOKEN")
 }
 
 create-release() {
@@ -16,11 +16,14 @@ create-release() {
 	git tag -a "$tag_name" -m "Production release at $(date -u)"
 
 	local release_body=$tag_name
-	if [ $old_tag != "null" ]; then
+	if [ "$old_tag" != "null" ]; then
+		echo "Building change log between $old_tag and $tag_name"
 		release_body=$(change-log $old_tag $tag_name)
+		echo $release_body
 	fi
 
 	git push $REMOTE --tags
+	echo "Publishing $tag_name as a release"
 	local release="$(printf "$release_json" "$tag_name" "$tag_name" "master" "$release_body")"
 	curl -s -d "$release" "$release_url?access_token=$GITHUB_ACCESS_TOKEN"
 }
